0265fc2024103c115f2afb12e89ba49325bb8f80
yesmineee <43929137+yesminee@users.noreply.github.com>
2023-02-13 10:12:19 +0100

    documentation

--- Backend/index.js
+++ Backend/index.js
@@ -4,16 +4,23 @@ const routes = require('./routes/routes');
 
 const port = 8000;
 
+//initialiser express
 const app = express();
 
+//middleware pour permettre d'echanger avec un api dans le localhost
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.use('/api', routes)
+//creation du chemin racine des apis 
+//tous les apis commencereont par /api
+app.use('/api', routes);
+
+//les donnés echangées seront sous format json
 app.use(express.json());
 
+//instatiation du serveur au port 8000
 app.listen(port, () => {
     console.log(`Server Started at ${port}`)
 })
