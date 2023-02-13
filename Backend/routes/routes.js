3ae2d7cec8e79b6413a7eccdaa58fb6cd52664c6
yesmineee <43929137+yesminee@users.noreply.github.com>
2023-02-13 10:14:24 +0100

    documentation

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const jsonData = require('./json');
 
+//instanciation du reouter
 const router = express.Router();
 
+//creation de l'api /getData qui retourne le fichier json jsonData
 router.get('/getData', async (req, res) => {
     try{
         res.json(jsonData);
@@ -12,4 +14,5 @@ router.get('/getData', async (req, res) => {
     }
 })
 
+//rendre l'api accessible dans tous le projet
 module.exports = router;
